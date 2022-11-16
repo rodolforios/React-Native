01905251b23f0cac5a9bf97bce71536056e9bffa
Rodolfo Rios <52710229+rodolforios@users.noreply.github.com>
2022-11-16 10:02:58 -0300

    .

--- meu_projeto/App.js
+++ meu_projeto/App.js
@@ -1,21 +1,28 @@
+import axios from "axios";
 import React from "react";
 //import { StatusBar } from 'expo-status-bar';
 import { Text, View } from "react-native";
 import Header from "./src/components/Header";
 
 export default class App extends React.Component {
-
   renderList() {
+   
 
-    const names = ["Eddie Van Halen", "Jimi Hendrix", "Chimbinha", "Steve Vai"];
-    
+    // const textElements = names.map((name) => {
+    //   return <Text key={name}>{name}</Text>;
+    // });
 
-    const textElements = names.map(name=>{
-      return <Text key={name}>{name}</Text>
-    })
+    /*Promises*/
 
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=5")
+      .then((response) => {
+        const { results } = response.data;
+        const names = results.map(people => people.name.first)
+        })
+      };
 
-    return textElements
+   
   }
   render() {
     return (
